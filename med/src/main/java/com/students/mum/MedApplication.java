b5b6ed8ae9e0dfdba897157de2af60c8fe2c8602
dongwang <dongwang@wangdongMacBook-Pro.local>
2019-05-11 22:23:52 -0500

    Created a starter project

---
+++ med/src/main/java/com/students/mum/MedApplication.java
@@ -0,0 +1,13 @@
+package com.students.mum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedApplication.class, args);
+	}
+
+}
---
+++ med/src/test/java/com/students/mum/MedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.students.mum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
